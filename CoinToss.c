7bf7e982ef80dcccac6ab2cf132449f2ff5ca8c3
s1270146 <[s1270146@gmail.com]>
2020-08-06 16:56:04 +0900

    HoT: Initial revision

---
+++ CoinToss.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#define N 3
+
+int main(){
+  int i,cnt=0;
+  printf("Tossing a coin...\n");
+  for(i=0;i<N;i++){
+    srand((unsigned int)time(NULL));
+    printf("Round %d:",i+1);
+    if(rand() % 2 +1 == 1){
+      printf("Heads\n");
+      cnt++;
+    }
+    else {
+      printf("Tails\n");
+    }
+    sleep(1);
+  }
+  printf("Heads:%d,Tails:%d\n",cnt,3-cnt);
+
+  return 0;
+}
